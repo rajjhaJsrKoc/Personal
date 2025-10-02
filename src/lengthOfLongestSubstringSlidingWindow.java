52017e24ff062fd9020904986aca4baccc8d9ab4
rajjhaJsrKoc <rajat948@gmail.com>
2025-10-02 17:09:34 +0530

    return the string

--- src/lengthOfLongestSubstringSlidingWindow.java
+++ src/lengthOfLongestSubstringSlidingWindow.java
@@ -5,6 +5,7 @@ public class lengthOfLongestSubstringSlidingWindow {
     public static void main(String[] args) {
         String subArray = "abcddcfda";
         System.out.println(lengthOfLongestSubstringSlidingWindow(subArray));
+        System.out.println(longestSubstring(subArray));
 
     }
 
@@ -24,6 +25,26 @@ public class lengthOfLongestSubstringSlidingWindow {
             maxLength = Math.max(maxLength, right - left + 1);
         }
         return maxLength;
+    }
+
+    public  static String longestSubstring(String s) {
+        int n = s.length();
+        HashSet<Character> seen = new HashSet<>();
+        int maxLength = 0;
+        int left = 0;
+        int start = 0;
+        for (int right = 0; right < n; right++) {
+            while (seen.contains(s.charAt(right))) {
+                seen.remove(s.charAt(left));
+                left++;
+            }
+            seen.add(s.charAt(right));
+            if (right - left + 1 > maxLength) {
+                maxLength = right - left + 1;
+                start = left; // update best window start
+            }
+        }
+        return s.substring(start, start + maxLength);
     }
 
 // JUST RETRUN THE SUBSTRING

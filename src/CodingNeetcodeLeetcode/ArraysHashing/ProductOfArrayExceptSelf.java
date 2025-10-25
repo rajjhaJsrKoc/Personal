3154143e75b46a750a715b619c46e9b8cb9642e5
rajjhaJsrKoc <rajat948@gmail.com>
2025-10-25 20:27:14 +0530

    new way of product of self

--- src/CodingNeetcodeLeetcode/ArraysHashing/ProductOfArrayExceptSelf.java
+++ src/CodingNeetcodeLeetcode/ArraysHashing/ProductOfArrayExceptSelf.java
@@ -6,6 +6,28 @@ public class ProductOfArrayExceptSelf {
     public static void main(String[] args) {
         int[] ars = {1, 2, 3, 4};
         productOfArray(ars);
+        productOfArrayWith1Space(ars);
+    }
+    private static void productOfArrayWith1Space(int[] ars){
+        int zeros =0,indx= 0, product= 1;
+        for (int i = 0; i < ars.length;i ++){
+            if (ars[i]==0){
+                zeros++;
+                indx =i;
+            }else {
+                product = product*ars[i];
+            }
+        }
+        int[] res = new int[ars.length];
+        Arrays.fill(res, 0);
+        if(zeros == 0){
+            for (int i = 0; i< ars.length; i ++){
+                res[i] = product/ars[i];
+            }
+        }else if (zeros==1){
+            res[indx] = product;
+        }
+        Arrays.stream(res).forEach(System.out::println);
     }
 
     private static void productOfArray(int[] ars) {

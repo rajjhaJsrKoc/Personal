a82a4f8321f6464f05ef378b3ffc1a479d13b72a
rajjhaJsrKoc <rajat948@gmail.com>
2025-11-08 18:00:23 +0530

    some comments

--- src/CelebrityProblem.java
+++ src/CelebrityProblem.java
@@ -29,4 +29,35 @@ public class CelebrityProblem {
         else
             System.out.println("No celebrity found");
         }
+    public static int celebrity(int[][] M) {
+    // Size of the given matrix
+    int n = M.length;
+
+    // To store count of people who know person of index i
+    int[] knowMe = new int[n];
+
+    // To store count of people who the person of index i knows
+    int[] Iknow = new int[n];
+
+    // Traverse the matrix to calculate knowMe and Iknow
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+
+            // If person i knows person j
+            if (M[i][j] == 1) {
+                knowMe[j]++;  // Person j is known by person i
+                Iknow[i]++;   // Person i knows person j
+            }
+        }
+    }
+
+    // Traverse all persons to find the celebrity
+    for (int i = 0; i < n; i++) {
+        // If person i knows no one and is known by everyone else
+        if (knowMe[i] == n - 1 && Iknow[i] == 0) {
+            return i;  // Person i is the celebrity
+        }
+    }
+        return -1;
+    }
 }

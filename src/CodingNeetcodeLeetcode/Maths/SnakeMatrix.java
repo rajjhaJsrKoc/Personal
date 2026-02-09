6ac9f61b27d6127390ef17d66dd6e390377345ae
rajjhaJsrKoc <rajat948@gmail.com>
2026-02-09 12:37:31 +0530

    rename it

--- src/CodingNeetcodeLeetcode/Maths/SnakeMatrix.java
+++ src/CodingNeetcodeLeetcode/Maths/SnakeMatrix.java
@@ -2,7 +2,7 @@ package CodingNeetcodeLeetcode.Maths;
 
 import java.util.Arrays;
 
-public class SpiralMatrix {
+public class SnakeMatrix {
     public static void main(String[] args){
         int[][] arr = {{1,2,3},//0
                 {4,5,6},//1
